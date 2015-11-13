1cdbeeb7827c277368ff00737c5aa0edb795b9ea
Godfrey Yeung <godfrey@placemeter.com>
2015-11-13 13:05:10 -0800

    beginning d3 viz

---
+++ graphvizAPI.js
@@ -0,0 +1,13 @@
+/* graph visualization API */
+
+
+// spawn classes of which node is instance of
+
+// spawn node parent classes
+
+// create a node with name and uri properties
+
+// create a link between two nodes
+
+// how to get a node's children?
+
